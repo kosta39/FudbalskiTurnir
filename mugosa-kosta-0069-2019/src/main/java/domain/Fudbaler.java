4a1c2d7d741679950db9d9df2d76f6bf3a4c408f
mugosa-kosta <kokson65@gmail.com>
2023-08-15 18:37:21 +0200

    Uradjena dokumentacija za klasu Fudbaler.

--- mugosa-kosta-0069-2019/src/main/java/domain/Fudbaler.java
+++ mugosa-kosta-0069-2019/src/main/java/domain/Fudbaler.java
@@ -10,28 +10,56 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
+ *Predstavlja fudbalera koji ucestvuje na turniru. Ima svoj id, ime, prezime i broj godina.
  *
+ * Fudbaler nasledjuje AbstractDomainObject klasu i samim tim implementira njene
+ * metode
  * @author Kosta
  */
 public class Fudbaler extends AbstractDomainObject {
-
+	/**
+	 * Id fudbalera kao Long
+	 */
     private Long fudbalerID;
+    /**
+     * Ime fudbalera kao String
+     */
     private String imeFudbalera;
-    private String prezimeFudbalera;
+    /**
+     * Prezime fudbalera kao String
+     */
+   private String prezimeFudbalera;
+   /**
+    * Godine fudbalera kao cijeli broj.
+    */
     private int godine;
-
+    /**
+     * Vraca String sa imenom i prezimenom fudbalera.
+     * @return ime i prezime fudbalera kao String.
+     */
     @Override
     public String toString() {
         return imeFudbalera + " " + prezimeFudbalera;
     }
-
+    /**
+     * Parametrizovani konstruktor koji postavlja vrijednost atributa fudbalera na
+     * one koji su dati kao parametri.
+     * @param fudbalerID vrijednost koja predstavlja id fudbalera
+     * @param imeFudbalera vrijednost koja predstavlja ime fudbalera
+     * @param prezimeFudbalera vrijednost koja predstavlja prezime fudbalera
+     * @param godine vrijednost koja predstavlja broj godina fudbalera
+     */
     public Fudbaler(Long fudbalerID, String imeFudbalera, String prezimeFudbalera, int godine) {
         this.fudbalerID = fudbalerID;
         this.imeFudbalera = imeFudbalera;
         this.prezimeFudbalera = prezimeFudbalera;
         this.godine = godine;
     }
-
+    /**
+     * /**
+     * Prazan konstruktor koji postavlja vrijednosti atributa fudbalera na pocetne
+     * koje se odnose na njihov tip podatka.
+     */
     public Fudbaler() {
     }
 
@@ -90,35 +118,59 @@ public class Fudbaler extends AbstractDomainObject {
     public String uslov() {
         return "";
     }
-
+    /**
+     * Vraca id fudbalera.
+     * @return id fudbalera kao Long
+     */
     public Long getFudbalerID() {
         return fudbalerID;
     }
-
+    /**
+     * Postavlja vrijednost za id fudbalera.
+     * @param fudbalerID nova vrijednost za id fudbalera.
+     */
     public void setFudbalerID(Long fudbalerID) {
         this.fudbalerID = fudbalerID;
     }
-
+    /**
+     * Vraca ime fudbalera.
+     * @return ime fudbalera kao String
+     */
     public String getImeFudbalera() {
         return imeFudbalera;
     }
-
+    /**
+     * Postavlja vrijednost za ime fudbalera.
+     * @param imeFudbalera nova vrijednost za ime fudbalera.
+     */
     public void setImeFudbalera(String imeFudbalera) {
         this.imeFudbalera = imeFudbalera;
     }
-
+    /**
+     * Vraca prezime fudbalera.
+     * @return prezime fudbalera kao String
+     */
     public String getPrezimeFudbalera() {
         return prezimeFudbalera;
     }
-
+    /**
+     * Postavlja vrijednost za prezime fudbalera.
+     * @param prezimeFudbalera nova vrijednost za prezime fudbalera.
+     */
     public void setPrezimeFudbalera(String prezimeFudbalera) {
         this.prezimeFudbalera = prezimeFudbalera;
     }
-
+    /**
+     * Vraca broj godina fudbalera.
+     * @return broj godina fudbalera kao Integer
+     */
     public int getGodine() {
         return godine;
     }
-
+    /**
+     * Postavlja vrijednost za godine fudbalera.
+     * @param godine nova vrijednost za godine fudbalera.
+     */
     public void setGodine(int godine) {
         this.godine = godine;
     }
