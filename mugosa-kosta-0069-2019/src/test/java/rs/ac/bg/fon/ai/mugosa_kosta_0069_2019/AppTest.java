83beb98bd34daa763bffb8ace2847e56f0a7037a
mugosa-kosta <kokson65@gmail.com>
2023-08-15 16:12:16 +0200

    Kreiranje projekta

---
+++ mugosa-kosta-0069-2019/src/test/java/rs/ac/bg/fon/ai/mugosa_kosta_0069_2019/AppTest.java
@@ -0,0 +1,20 @@
+package rs.ac.bg.fon.ai.mugosa_kosta_0069_2019;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
