3dc1ed9bf51d86066521ce78ad29cd6f48f28493
mugosa-kosta <kokson65@gmail.com>
2023-08-18 11:35:36 +0200

    Zavrsena dokumentacija za klasu TableModelTimovi.

--- mugosa-kosta-0069-2019/src/main/java/models/TableModelTimovi.java
+++ mugosa-kosta-0069-2019/src/main/java/models/TableModelTimovi.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import domain.Tim;
@@ -13,15 +8,28 @@ import javax.swing.table.AbstractTableModel;
 import so.SOGetAllTim;
 
 /**
- *
+ * Klasa koja sluzi za kreiranje specificnog modela tabele timova i koristi se prilikom
+ * pretrage timova.
+ * 
  * @author Kosta
  */
 public class TableModelTimovi extends AbstractTableModel implements Runnable {
-
+	/**
+	 * Lista postojecih timova.
+	 */
     private ArrayList<Tim> lista;
+    /**
+     * Nazivi kolona tabele dati preko String-a
+     */
     private String[] kolone = {"ID", "Naziv"};
+    /**
+     * Parametar na osnovu koga se vrsi sama pretraga dat kao String.
+     */
     private String parametar = "";
-
+    /**
+     * Neparametrizovan konstruktor koji sluzi za pozivanje sistemske operacije koja vraca
+     * postojece timove iz baze i prikazuje ih u tabeli za pretragu.
+     */
     public TableModelTimovi() {
         try {
             SOGetAllTim so=new SOGetAllTim();
@@ -31,22 +39,38 @@ public class TableModelTimovi extends AbstractTableModel implements Runnable {
             Logger.getLogger(TableModelTimovi.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    /**
+     * Vraca broj redova tabele.
+     * @return broj redova tabele kao cijeli broj.
+     */
     @Override
     public int getRowCount() {
         return lista.size();
     }
-
+    /**
+     * Vraca broj kolona tabele
+     * @return broj kolona tabele kao cijeli broj.
+     */
     @Override
     public int getColumnCount() {
         return kolone.length;
     }
-
+    /**
+     * Vraca naziv i-te kolone tabele.
+     * @param i broj kolone tabele ciji naziv treba da se vrati.
+     * @return naziv i-te kolone tabele kao String.
+     */
     @Override
     public String getColumnName(int i) {
         return kolone[i];
     }
-
+    /**
+     * Vraca vrijednost polja u tabeli.
+     * 
+     * @param row red u tabeli u kojem se nalazi dato polje.
+     * @param column kolona u tabeli u kojoj se nalazi dato polje.
+     * @return vrijednost trazenog polja u tabeli kao Object.
+     */
     @Override
     public Object getValueAt(int row, int column) {
         Tim t = lista.get(row);
@@ -61,11 +85,17 @@ public class TableModelTimovi extends AbstractTableModel implements Runnable {
                 return null;
         }
     }
-
+    /**
+     * Vraca tim koji se nalazi u trazenom redu tabele.
+     * @param row broj trazenog reda tabele
+     * @return tim koji se nalazi u trazenom redu tabele.
+     */
     public Tim getSelectedTim(int row) {
         return lista.get(row);
     }
-
+    /**
+     * Sluzi za osvjezaavanje(refresh) tabele nakon odredjenog broja sekundi(u ovom slucaju 10).
+     */
     @Override
     public void run() {
         try {
@@ -77,12 +107,22 @@ public class TableModelTimovi extends AbstractTableModel implements Runnable {
             Logger.getLogger(TableModelTimovi.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    /**
+     * Sluzi za postavljanje vrijednosti parametra na osnovu kog se vrsi pretraga timova.
+     * @param parametar vrijednost na osnovu koje se vrsi pretraga timova.
+     */
     public void setParametar(String parametar) {
         this.parametar = parametar;
         refreshTable();
     }
-
+    /**
+     * Sluzi za azuriranje stanja tabele nakon sto se unese parametar za pretragu ili u slucaju
+     * izmjene ili brisanja nekog tima.
+     * 
+     * Prvo se poziva sistemska operacija kojoj se iz baze vracaju svi postojeci timovi, nakon cega se
+     * provjerava da li je unijet parametar. Ako je to slucaj poredi naziv tima sa vrijednoscu parametra
+     * i nakon toga ubacuje u tabelu samo one timove kod kojih postoji jednakost.
+     */
     public void refreshTable() {
         try {
             SOGetAllTim so=new SOGetAllTim();
