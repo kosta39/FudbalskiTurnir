7442e96f4ee85f2a8b734a969cbeccf717d99bc3
mugosa-kosta <kokson65@gmail.com>
2023-08-18 10:58:38 +0200

    Zavrsena dokumentacija za klasu SODeleteTim.

--- mugosa-kosta-0069-2019/src/main/java/so/SODeleteTim.java
+++ mugosa-kosta-0069-2019/src/main/java/so/SODeleteTim.java
@@ -11,18 +11,26 @@ import domain.Tim;
 import so.AbstractSO;
 
 /**
+ *Sistemska operacija koja sluzi za brisanje postojeceg tima iz baze. Nasledjuje apstraktnu
+ *klasu AbstractSO pa samim tim i implementira njene apstraktne metode.
  *
  * @author Kosta
  */
 public class SODeleteTim extends AbstractSO {
-
+	/**
+	 * Vrsi validaciju za instancu objekta.
+	 * @throws Exception ukoliko prosledjeni objekat nije instanca klase Tim.
+	 */
     @Override
     protected void validate(AbstractDomainObject ado) throws Exception {
         if (!(ado instanceof Tim)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Tim!");
         }
     }
-
+    /**
+     * Poziva se broker baze podataka koji vrsi DELETE upit kojim se brise prosledjeni
+     * objekat tima iz baze.
+     */
     @Override
     protected void execute(AbstractDomainObject ado) throws Exception {
         DBBroker.getInstance().delete(ado);
