e22c5758572ecc34b161bb71e3af35cf7a8f161f
mugosa-kosta <kokson65@gmail.com>
2023-08-18 10:41:51 +0200

    Zavrsena dokumentacija za klasu SOGetAllTim.

--- mugosa-kosta-0069-2019/src/main/java/so/SOGetAllTim.java
+++ mugosa-kosta-0069-2019/src/main/java/so/SOGetAllTim.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package so;
 
 import db.DBBroker;
@@ -12,26 +7,38 @@ import java.util.ArrayList;
 import so.AbstractSO;
 
 /**
- *
+ *Predstavlja sistemsku operaciju koja se koristi za ucitavanje postojecih timova
+ * iz baze podataka. Implementira apstraktne metode klase AbstractSO koju nasledjuje
  * @author Kosta
  */
 public class SOGetAllTim extends AbstractSO {
-
+	/**
+	 * Lista svih timova u bazi.
+	 */
     private ArrayList<Tim> lista;
-
+    /**
+     * Vrsi se validacija za instancu objekta
+     * @throws Exception ukoliko prosledjeni objekat nije instanca klase Tim
+     */
     @Override
     protected void validate(AbstractDomainObject ado) throws Exception {
         if (!(ado instanceof Tim)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Tim!");
         }
     }
-
+    /**
+     * Poziva se broker baze podataka koji vrsi SELECT upit i rezultat smjesta u
+     * listu koja sadrzi sve timove.
+     */
     @Override
     protected void execute(AbstractDomainObject ado) throws Exception {
         ArrayList<AbstractDomainObject> timovi = DBBroker.getInstance().select(ado);
         lista = (ArrayList<Tim>) (ArrayList<?>) timovi;
     }
-
+    /**
+     * Vraca listu sa postojecim timovima.
+     * @return lista sa postojecim timovima.
+     */
     public ArrayList<Tim> getLista() {
         return lista;
     }
