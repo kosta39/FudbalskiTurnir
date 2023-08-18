ca20c979c6467030da1c7a3e6e65034bb9cd554d
mugosa-kosta <kokson65@gmail.com>
2023-08-18 10:57:36 +0200

    Zavrsena dokumentacija za klasu SODeleteTurnir.

--- mugosa-kosta-0069-2019/src/main/java/so/SODeleteTurnir.java
+++ mugosa-kosta-0069-2019/src/main/java/so/SODeleteTurnir.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package so;
 
 import db.DBBroker;
@@ -11,18 +6,26 @@ import domain.Turnir;
 import so.AbstractSO;
 
 /**
+ *Sistemska operacija koja sluzi za brisanje postojeceg turnira iz baze. Nasledjuje apstraktnu
+ *klasu AbstractSO pa samim tim i implementira njene apstraktne metode.
  *
  * @author Kosta
  */
 public class SODeleteTurnir extends AbstractSO {
-
+	/**
+	 * Vrsi validaciju za instancu objekta.
+	 * @throws Exception ukoliko prosledjeni objekat nije instanca klase Turnir
+	 */
     @Override
     protected void validate(AbstractDomainObject ado) throws Exception {
         if (!(ado instanceof Turnir)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Turnir!");
         }
     }
-
+    /**
+     * Poziva se broker baze podataka koji vrsi DELETE upit kojim se brise prosledjeni
+     * objekat turnira iz baze.
+     */
     @Override
     protected void execute(AbstractDomainObject ado) throws Exception {
         DBBroker.getInstance().delete(ado);
