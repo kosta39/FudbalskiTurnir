d2f5517687ae76bbd233554319e877db6fcda4c0
mugosa-kosta <kokson65@gmail.com>
2023-08-18 10:40:24 +0200

    Zavrsena dokumentacija za klasu SOGetAllTurnir.

--- mugosa-kosta-0069-2019/src/main/java/so/SOGetAllTurnir.java
+++ mugosa-kosta-0069-2019/src/main/java/so/SOGetAllTurnir.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package so;
 
 import db.DBBroker;
@@ -12,26 +7,38 @@ import java.util.ArrayList;
 import so.AbstractSO;
 
 /**
- *
+ *Predstavlja sistemsku operaciju koja se koristi za ucitavanje postojecih turnira
+ * iz baze podataka. Implementira apstraktne metode klase AbstractSO koju nasledjuje
  * @author Kosta
  */
 public class SOGetAllTurnir extends AbstractSO {
-
+	/**
+	 * Lista svih turnira u bazi.
+	 */
     private ArrayList<Turnir> lista;
-
+    /**
+     * Vrsi se validacija za instancu objekta
+     * @throws Exception ukoliko prosledjeni objekat nije instanca klase Turnir
+     */
     @Override
     protected void validate(AbstractDomainObject ado) throws Exception {
         if (!(ado instanceof Turnir)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Turnir!");
         }
     }
-
+    /**
+     * Poziva se broker baze podataka koji vrsi SELECT upit i rezultat smjesta u
+     * listu koja sadrzi sve turnire.
+     */
     @Override
     protected void execute(AbstractDomainObject ado) throws Exception {
         ArrayList<AbstractDomainObject> turniri = DBBroker.getInstance().select(ado);
         lista = (ArrayList<Turnir>) (ArrayList<?>) turniri;
     }
-
+    /**
+     * Vraca listu sa postojecim turnirima.
+     * @return lista sa postojecim turnirima.
+     */
     public ArrayList<Turnir> getLista() {
         return lista;
     }
