a5c4b0a9c534f8940fb7283bdd8e81270c1953e9
mugosa-kosta <kokson65@gmail.com>
2023-08-18 11:29:27 +0200

    Zavrsena dokumentacija za klasu TableModelTurnir

--- mugosa-kosta-0069-2019/src/main/java/models/TableModelTurniri.java
+++ mugosa-kosta-0069-2019/src/main/java/models/TableModelTurniri.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import domain.Turnir;
@@ -14,15 +9,28 @@ import javax.swing.table.AbstractTableModel;
 import so.SOGetAllTurnir;
 
 /**
- *
+ * Klasa koja sluzi za kreiranje specificnog modela tabele turnira i koristi se prilikom
+ * pretrage turnira.
+ * 
  * @author Kosta
  */
 public class TableModelTurniri extends AbstractTableModel implements Runnable {
-
+	/**
+	 * Lista postojecih turnira.
+	 */
     private ArrayList<Turnir> lista;
+    /**
+     * Nazivi kolona tabele dati preko String-a
+     */
     private String[] kolone = {"ID", "Naziv", "Grad", "Datum od", "Datum do"};
+    /**
+     * Parametar na osnovu koga se vrsi sama pretraga dat kao String
+     */
     private String parametar = "";
-
+    /**
+     * Neparametrizovan konstruktor koji sluzi za pozivanje sistemske operacije koja vraca
+     * postojece turnire iz baze i prikazuje ih u tabeli za pretragu.
+     */
     public TableModelTurniri() {
         try {
             SOGetAllTurnir so=new SOGetAllTurnir();
@@ -32,22 +40,38 @@ public class TableModelTurniri extends AbstractTableModel implements Runnable {
             Logger.getLogger(TableModelTurniri.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    /**
+     * Vraca broj redova tabele.
+     * @return broj redova tabele kao cijeli broj.
+     */
     @Override
     public int getRowCount() {
         return lista.size();
     }
-
+    /**
+     * Vraca broj kolona tabele
+     * @return broj kolona tabele kao cijeli broj.
+     */
     @Override
     public int getColumnCount() {
         return kolone.length;
     }
-
+    /**
+     * Vraca naziv i-te kolone tabele.
+     * @param i broj kolone tabele ciji naziv treba da se vrati.
+     * @return naziv i-te kolone tabele kao String.
+     */
     @Override
     public String getColumnName(int i) {
         return kolone[i];
     }
-
+    /**
+     * Vraca vrijednost polja u tabeli.
+     * 
+     * @param row red u tabeli u kojem se nalazi dato polje.
+     * @param column kolona u tabeli u kojoj se nalazi dato polje.
+     * @return vrijednost trazenog polja u tabeli kao Object.
+     */
     @Override
     public Object getValueAt(int row, int column) {
         Turnir t = lista.get(row);
@@ -69,11 +93,17 @@ public class TableModelTurniri extends AbstractTableModel implements Runnable {
                 return null;
         }
     }
-
+    /**
+     * Vraca turnir koji se nalazi u trazenom redu tabele.
+     * @param row broj trazenog reda tabele
+     * @return turnir koji se nalazi u trazenom redu tabele.
+     */
     public Turnir getSelectedTurnir(int row) {
         return lista.get(row);
     }
-
+    /**
+     * Sluzi za osvjezaavanje(refresh) tabele nakon odredjenog broja sekundi(u ovom slucaju 10).
+     */
     @Override
     public void run() {
         try {
@@ -85,12 +115,22 @@ public class TableModelTurniri extends AbstractTableModel implements Runnable {
             Logger.getLogger(TableModelTurniri.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    /**
+     * Sluzi za postavljanje vrijednosti parametra na osnovu kog se vrsi pretraga turnira.
+     * @param parametar vrijednost na osnovu koje se vrsi pretraga turnira.
+     */
     public void setParametar(String parametar) {
         this.parametar = parametar;
         refreshTable();
     }
-
+    /**
+     * Sluzi za azuriranje stanja tabele nakon sto se unese parametar za pretragu ili u slucaju
+     * izmjene ili brisanja nekog turnira.
+     * 
+     * Prvo se poziva sistemska operacija kojoj se iz baze vracaju svi postojeci turniri, nakon cega se
+     * provjerava da li je unijet parametar. Ako je to slucaj poredi naziv turnira sa vrijednoscu parametra
+     * i nakon toga ubacuje u tabelu samo one turnire kod kojih postoji jednakost.
+     */
     public void refreshTable() {
         try {
             SOGetAllTurnir so=new SOGetAllTurnir();
