575ff743050165de92919987fe853161148fecf0
mugosa-kosta <kokson65@gmail.com>
2023-08-18 11:43:59 +0200

    Zavrsena dokumentacija za klasu TableModelUtakmice.

--- mugosa-kosta-0069-2019/src/main/java/models/TableModelUtakmice.java
+++ mugosa-kosta-0069-2019/src/main/java/models/TableModelUtakmice.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import domain.Turnir;
@@ -14,20 +9,35 @@ import javax.swing.table.AbstractTableModel;
 import so.SOGetAllUtakmica;
 
 /**
- *
+ * Klasa koja sluzi za kreiranje modela tabele utakmica koje su odigrane na nekom turniru.
+ * Koristi se prilikom dodavanja novog turnira ili izmjene postojeceg.
  * @author Kosta
  */
 public class TableModelUtakmice extends AbstractTableModel {
-
+	/**
+	 * Lista sa utakmicama koje su odigrane na datom turniru.
+	 */
     private ArrayList<Utakmica> lista;
+    /**
+     * Nazivi kolona u tabeli kao String
+     */
     private String[] kolone = {"Prvi tim", "Drugi tim", "Broj golova prvog", "Broj golova drugog",
         "Pobednik"};
+    /**
+     * Redni broj utakmice kao cijeli broj.
+     */
     private int rb = 0;
-
+    /**
+     * Prazan konstruktor koji sluzi za inicijalizaciju liste utakmica.
+     */
     public TableModelUtakmice() {
         lista = new ArrayList<>();
     }
-
+    /**
+     * Konstruktor koji sluzi za pozivanje sistemske operacije koja vraca sve utakmice datog
+     * turnira iz baze koji je unijet kao parametar.
+     * @param t tunrir iz baze na osnovu kog se ucitavaju sve utakmice.
+     */
     public TableModelUtakmice(Turnir t) {
         try {
             SOGetAllUtakmica so=new SOGetAllUtakmica();
@@ -37,22 +47,38 @@ public class TableModelUtakmice extends AbstractTableModel {
             Logger.getLogger(TableModelUtakmice.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    /**
+     * Vraca broj redova tabele.
+     * @return broj redova tabele kao cijeli broj.
+     */
     @Override
     public int getRowCount() {
         return lista.size();
     }
-
+    /**
+     * Vraca broj kolona tabele
+     * @return broj kolona tabele kao cijeli broj.
+     */
     @Override
     public int getColumnCount() {
         return kolone.length;
     }
-
+    /**
+     * Vraca naziv i-te kolone tabele.
+     * @param i broj kolone tabele ciji naziv treba da se vrati.
+     * @return naziv i-te kolone tabele kao String.
+     */
     @Override
     public String getColumnName(int i) {
         return kolone[i];
     }
-
+    /**
+     * Vraca vrijednost polja u tabeli.
+     * 
+     * @param row red u tabeli u kojem se nalazi dato polje.
+     * @param column kolona u tabeli u kojoj se nalazi dato polje.
+     * @return vrijednost trazenog polja u tabeli kao Object.
+     */
     @Override
     public Object getValueAt(int row, int column) {
         Utakmica u = lista.get(row);
@@ -73,14 +99,20 @@ public class TableModelUtakmice extends AbstractTableModel {
                 return null;
         }
     }
-
+    /**
+     * Dodaje utakmicu u tabelu nakon cega je azurira
+     * @param u utakmica koja se dodaje u tabelu
+     */
     public void dodajUtakmicu(Utakmica u) {
         rb = lista.size();
         u.setRbUtakmice(++rb);
         lista.add(u);
         fireTableDataChanged();
     }
-
+    /**
+     * Brise utakmicu iz tabele nakon cega je azurira
+     * @param row red tabele utakmice koja se brise
+     */
     public void obrisiUtakmicu(int row) {
         lista.remove(row);
 
@@ -91,7 +123,10 @@ public class TableModelUtakmice extends AbstractTableModel {
 
         fireTableDataChanged();
     }
-
+    /**
+     * Vraca listu utakmica koje se odnose na dati turnir.
+     * @return lista utakmica koje se odnose na dati turnir.
+     */
     public ArrayList<Utakmica> getLista() {
         return lista;
     }
