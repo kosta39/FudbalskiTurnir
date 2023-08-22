0d331ebb2194cdf19dd35094070a12080ea5b7ae
mugosa-kosta <kokson65@gmail.com>
2023-08-22 10:34:43 +0200

    Testirana sistemska operacija za vracanje administratora.

---
+++ mugosa-kosta-0069-2019/test/so/SOGetAllAdministratorTest.java
@@ -0,0 +1,49 @@
+package so;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import domain.Administrator;
+import domain.Igrac;
+
+class SOGetAllAdministratorTest {
+	SOGetAllAdministrator so;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		so=new SOGetAllAdministrator();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		so=null;
+	}
+	@Test
+	void testNeuspesnaValidacijaPogresnaKlasa() {
+		assertThrows(Exception.class, () -> so.templateExecute(new Igrac()));
+	}
+	@Test
+	void testNeuspesnaValidacijaNullKlasa() {
+		assertThrows(Exception.class, () -> so.templateExecute(null));
+	}
+	@Test
+	void testUspesnoVracenaListaAdministratora() {
+		try {
+			so.templateExecute(new Administrator());
+			ArrayList<Administrator> vraceni = so.getLista();
+
+			assertEquals(2, vraceni.size());
+			assertTrue(vraceni.get(0).getUsername().equalsIgnoreCase("kosta") && 
+					vraceni.get(0).getPassword().equalsIgnoreCase("kosta123"));
+			assertTrue(vraceni.get(1).getUsername().equalsIgnoreCase("savo") && 
+					vraceni.get(1).getPassword().equalsIgnoreCase("savo123"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
