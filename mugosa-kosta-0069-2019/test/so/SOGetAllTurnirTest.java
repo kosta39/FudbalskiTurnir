8464c50c26608e1bbd24a7156a5abcb58228bcb9
mugosa-kosta <kokson65@gmail.com>
2023-08-22 12:01:21 +0200

    Testirana sistemska operacija za vracanje svih turnira.

---
+++ mugosa-kosta-0069-2019/test/so/SOGetAllTurnirTest.java
@@ -0,0 +1,48 @@
+package so;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import domain.Igrac;
+import domain.Tim;
+import domain.Turnir;
+
+class SOGetAllTurnirTest {
+	SOGetAllTurnir so;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		so=new SOGetAllTurnir();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		so=null;
+	}
+	@Test
+	void testNeuspesnaValidacijaPogresnaKlasa() {
+		assertThrows(Exception.class, () -> so.templateExecute(new Igrac()));
+	}
+	@Test
+	void testNeuspesnaValidacijaNullKlasa() {
+		assertThrows(Exception.class, () -> so.templateExecute(null));
+	}
+	void testUspesnoVracenaListaTurnira() {
+		try {
+			so.templateExecute(new Tim());
+			ArrayList<Turnir> vraceni = so.getLista();
+
+			assertEquals(3, vraceni.size());
+			assertTrue(vraceni.get(0).getNazivTurnira().equalsIgnoreCase("Letnji turnir"));
+			assertTrue(vraceni.get(1).getNazivTurnira().equalsIgnoreCase("Turnir 4"));
+			assertTrue(vraceni.get(2).getNazivTurnira().equalsIgnoreCase("Kakice"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
